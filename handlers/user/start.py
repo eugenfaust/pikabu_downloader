50ee937866b07f379a4929f48bc23d4193f02a79
eugenfaust <108708484+eugenfaust@users.noreply.github.com>
2022-11-19 05:55:56 +0300

    add caption for photos

--- handlers/user/start.py
+++ handlers/user/start.py
@@ -33,6 +33,7 @@ async def pikabu_link_handler(msg: types.Message, bot):
         title = await pikabu.parse_title()
         video, file_size, duration = await pikabu.parse_video()
         images = await pikabu.parse_images()
+        caption = '<a href="{}">{}</a>'.format(link, title)
         if msg.chat.type != 'private':
             try:
                 await msg.delete()
@@ -43,7 +44,7 @@ async def pikabu_link_handler(msg: types.Message, bot):
             temp_group = []
             counter = 0
             for img in images:
-                temp_group.append(InputMediaPhoto(media=img))
+                temp_group.append(InputMediaPhoto(media=img, caption=caption))
                 counter += 1
                 if counter >= 10:
                     media_groups.append(temp_group)
@@ -52,21 +53,21 @@ async def pikabu_link_handler(msg: types.Message, bot):
             for media in media_groups:
                 await msg.answer_media_group(media)
         else:
-            await msg.answer_photo(images[0])
+            await msg.answer_photo(images[0], caption=caption)
         if not video:
             return
         try:
             if 50_000_000 >= file_size >= 20_000_000:  # 20 MB by link limit, 50 MB upload limit
                 input_file = URLInputFile(video)
-                sent = await msg.answer_video(input_file, caption='<a href="{}">{}</a>'.format(link, title),
+                sent = await msg.answer_video(input_file, caption=caption,
                                               duration=duration)
             elif file_size >= 50_000_000:
                 return
             else:
                 sent = await msg.answer_video(video,
-                                              caption='<a href="{}">{}</a>'.format(link, title))
+                                              caption=caption)
             await bot.send_video(config.CHANNEL_ID, sent.video.file_id,
-                                 caption='<a href="{}">{}</a>'.format(link, title))
+                                 caption=caption)
         except Exception as e:
             await bot.send_message(config.ADMIN_IDS[0],
                                    'Error in link:\n{}\n{}\nCan\'t upload video'.format(video, e))
